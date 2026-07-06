d8f04728d2640015a64f30ba3079834da54e54bf
eltouma <elsa@deltablot.email>
2026-07-06 12:33:05 +0200

    fix reset.sh

--- reset.sh
+++ reset.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -e
-./cli db reset
 docker compose down
 sudo rm -rf data/postgres
 docker compose up -d
 echo "waiting"
 sleep 5
-./cli db migrate up && ./cli db seed && ./cli admin add $1 && ./cli admin remove 0000-0000-0000-0000
+./cli db migrate up
+./cli db seed
+./cli admin add "$1"
+if [ "$2" ]
+then
+  ./cli admin remove "$2"
+fi
